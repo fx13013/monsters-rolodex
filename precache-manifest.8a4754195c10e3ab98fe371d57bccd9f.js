79c256d6c62aa8d137b113d2e0a807be72af2c78
Fx BTG <52882967+fx13013@users.noreply.github.com>
2020-04-01 17:31:29 +0200

    Updates

---
+++ precache-manifest.8a4754195c10e3ab98fe371d57bccd9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ed55ed2570f73ab34f4fd015fdd7bbb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "98e0cf1b56b7ee6e49b9",
+    "url": "/monsters-rolodex/static/css/main.d2e2f06a.chunk.css"
+  },
+  {
+    "revision": "696cd664e94359881443",
+    "url": "/monsters-rolodex/static/js/2.259342ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.259342ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98e0cf1b56b7ee6e49b9",
+    "url": "/monsters-rolodex/static/js/main.0d57a609.chunk.js"
+  },
+  {
+    "revision": "4ea3fb9132ee769ac0ba",
+    "url": "/monsters-rolodex/static/js/runtime-main.12e1afbb.js"
+  }
+]);
